5208dcfbd2480dd51aa10fb88abb27a272a98e5c
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-15 20:48:30 +0300

    DZ1 bag is "0" resolved

--- src/by/teachmeskills/Lessen1/DZ1.java
+++ src/by/teachmeskills/Lessen1/DZ1.java
@@ -2,26 +2,29 @@ package by.teachmeskills.Lessen1;
 
 public class DZ1 {
     public static void main(String[] args) {
-        int a = 400;
+        int a = 100;
         int i = 1;
         String znak=null;
-        if (a == 0) {
-            System.out.println("Число заданное в условии равно 0");
-        }
-            if (a < 0&&a!=0) {
+        if (a!=0){
+            if (a < 0) {
                 znak = "отрицательное";
                 a = -a;
             }
             else {
                 znak = "положительное";
             }
-
             while (a / 10 > 0) {
                 a = a / 10;
                 i++;
             }
-
             System.out.println("Число заданное в условии " + znak + " " + i + "-значное");
+
+        }
+        else {
+            System.out.println("Число заданное в условии равно 0");
+        }
+
+
 
         }
     }
