1468e115d92870b7ed51bcc02b797d57bde3f675
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-15 19:10:24 +0300

    add DZ2

--- src/by/teachmeskills/Lessen1/DZ2.java
+++ src/by/teachmeskills/Lessen1/DZ2.java
@@ -1,4 +1,20 @@
 package by.teachmeskills.Lessen1;
 
 public class DZ2 {
+    public static void main(String[] args) {
+        int a=5;
+        int b=5;
+        int c=5;
+
+        if (a<=0|b<=0|c<=0){
+            System.out.println("Задайте положительные числа");
+        }
+        else if((a+b)>c&(b+c)>a&(c+a)>b){
+            System.out.println("Треугольник с данными сторонами возможен");
+        }
+        else {
+            System.out.println("Треугольник с данными сторонами НЕвозможен");
+        }
+    }
+
 }
