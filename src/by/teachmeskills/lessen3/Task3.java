8a6fca8f828f30d5cdfeff560ae53f7615ab4857
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-16 23:13:22 +0300

    add task3

---
+++ src/by/teachmeskills/lessen3/Task3.java
@@ -0,0 +1,15 @@
+package by.teachmeskills.lessen3;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int chislo=10;
+        if (chislo>0){
+            chislo+=1;
+        }else if(chislo<0){
+            chislo-=2;
+        }else {
+            chislo=10;
+        }
+        System.out.println("Выводим полученное число: "+chislo);
+    }
+}
