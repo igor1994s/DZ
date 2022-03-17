66c85e777e1d8d48380d9ef4119f08fceb05a809
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-17 15:41:10 +0300

    4

---
+++ src/by/teachmeskills/lessen3/Task4.java
@@ -0,0 +1,20 @@
+package by.teachmeskills.lessen3;
+
+public class Task4 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=25;
+        int c=-34;
+        int count=0;
+        if (a>0){
+            count++;
+        }
+        if(b>0){
+            count++;
+        }
+        if(c>0){
+            count++;
+        }
+        System.out.println("Количество положительных чисел равно: "+count);
+    }
+}
