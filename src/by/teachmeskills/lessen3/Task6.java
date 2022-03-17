3bab67ec518c8a7eccb98a57cc835611635a5152
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-17 16:14:24 +0300

    add task6

---
+++ src/by/teachmeskills/lessen3/Task6.java
@@ -0,0 +1,15 @@
+package by.teachmeskills.lessen3;
+
+public class Task6 {
+    public static void main(String[] args) {
+        int a=190;
+        int b=190;
+        if(a>b){
+            System.out.println("Большее из чисел равно: "+a);
+        }else if(a<b){
+            System.out.println("Большее из чисел равно: "+b);
+        }else {
+            System.out.println("Числа равны");
+        }
+    }
+}
