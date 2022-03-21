b6ea154ea55476b6b8d753c4c0d907dda39de1e0
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-21 14:51:07 +0300

    L4task2 done

---
+++ src/by/teachmeskills/lessen4/L4Task2.java
@@ -0,0 +1,12 @@
+package by.teachmeskills.lessen4;
+
+public class L4Task2 {
+    public static void main(String[] args) {
+        int ameba=1;
+        for (int i = 1; i <9 ; i++) {
+            ameba=ameba*2;
+            System.out.println("Через "+i*3+" ч количество амеб будет: "+ameba);
+        }
+
+    }
+}
