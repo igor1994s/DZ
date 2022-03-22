e38ec8cc65e9a3c7cd7e99143a408a7277bb6f1d
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-22 14:36:34 +0300

    L5task4 done and update L5task3

--- src/by/teachmeskills/lessen5/L5Task4.java
+++ src/by/teachmeskills/lessen5/L5Task4.java
@@ -3,7 +3,7 @@ package by.teachmeskills.lessen5;
 import java.util.Random;
 import java.util.Scanner;
 
-public class L5Task3 {
+public class L5Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         Random r = new Random();
@@ -12,13 +12,18 @@ public class L5Task3 {
         int[] a = new int[n];
         int count = 0;
         for (int i = 0; i < n; i++) {
-            a[i] = r.nextInt(99);
+            a[i] = r.nextInt(20);
             System.out.print(a[i] + " ");
-            if (a[i] % 2 == 0) {
-                count++;
+            if (i % 2 == 1) {
+                a[i] = 0;
             }
         }
         System.out.println();
-        System.out.println("Количество четных элементов в массиве равно: " + count);
+        System.out.println("Итоговый массив: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(a[i] + " ");
+
+        }
+
     }
 }
