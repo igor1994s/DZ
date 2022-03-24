b494693c2ff3930dce8b375a236d0e756139339d
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-24 16:25:36 +0300

    L5task24 done

---
+++ src/by/teachmeskills/lessen5/L5Task24.java
@@ -0,0 +1,38 @@
+package by.teachmeskills.lessen5;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class L5Task24 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+        int index;
+        int sum=0;
+
+        System.out.println("Задайте размерность двухмерного массива: ");
+        index = sc.nextInt();
+        int a[][] = new int[index][index];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                a[i][j] = r.nextInt(50);
+            }
+        }
+        System.out.println("Данный массив: ");
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        for (int i = 0; i < a.length-1; i++) {
+            for (int j = 0; j <=a.length-2-i; j++) {
+                if(a[i][j]%2==0){
+                    sum+=a[i][j];
+                }
+            }
+        }
+        System.out.println("Сумма четных элементов над побочной диагональю(не включительно) равна: "+sum);
+    }
+}
