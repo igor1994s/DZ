352b4d31b7b3860cedb30b774884bb2f8f7be759
Igor Sergeychuk <igor1994s@yabdex.by>
2022-03-30 08:31:16 +0300

    Person done.

---
+++ src/by/teachmeskills/lessen6OOP/person/PersonApp.java
@@ -0,0 +1,12 @@
+package by.teachmeskills.lessen6OOP.person;
+
+public class PersonApp {
+    public static void main(String[] args) {
+        Person maksim=new Person();
+        maksim.setFullName("Maksim");
+        maksim.setAge(28);
+        Person vlad =new Person("Vlad",24);
+        maksim.move();
+        vlad.talk();
+    }
+}
