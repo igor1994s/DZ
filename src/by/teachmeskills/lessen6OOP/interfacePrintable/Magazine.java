03857576660c3247fa6130808e1702786a499273
Igor Sergeychuk <igor1994s@yabdex.by>
2022-04-14 15:42:52 +0300

    printable done

---
+++ src/by/teachmeskills/lessen6OOP/interfacePrintable/Magazine.java
@@ -0,0 +1,35 @@
+package by.teachmeskills.lessen6OOP.interfacePrintable;
+
+public class Magazine implements Printable {
+    private String name;
+
+    public Magazine(String name) {
+        this.name = name;
+    }
+
+    public Magazine() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void print() {
+        System.out.printf("Распечатать журнал с названием %s \n", name);
+    }
+
+    public static void printMagazins(Printable[] p) {
+        for (int i = 0; i < p.length; i++) {
+            if (p[i] instanceof Magazine) {
+                System.out.print(((Magazine) p[i]).getName() + " ");
+            }
+        }
+        System.out.println();
+    }
+}
+
---
+++ src/by/teachmeskills/lessen6OOP/interfacePrintable/Printable.java
@@ -0,0 +1,5 @@
+package by.teachmeskills.lessen6OOP.interfacePrintable;
+
+public interface Printable {
+    public void print();
+}
