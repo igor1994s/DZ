6d2edf7fcfe5919bc12ed55efc1b51a19543afb6
Igor Sergeychuk <igor1994s@yabdex.by>
2022-05-17 16:59:48 +0300

    Task2 of Lessen15 done.

---
+++ src/by/teachmeskills/lessen15/task2/People.java
@@ -0,0 +1,51 @@
+package by.teachmeskills.lessen15.task2;
+
+import java.util.ArrayList;
+
+public class People{
+    private String name;
+    private int age;
+    private Sex sex;
+
+    public People(String name, int age, Sex sex) {
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    public People() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public Sex getSex() {
+        return sex;
+    }
+
+    public void setSex(Sex sex) {
+        this.sex = sex;
+    }
+
+    @Override
+    public String toString() {
+        return "People{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", sex=" + sex +
+                '}';
+    }
+}
