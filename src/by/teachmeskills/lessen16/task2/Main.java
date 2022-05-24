ad3a6105c3dc641f36e05301b28a5d0a9b5da558
Igor Sergeychuk <igor1994s@yabdex.by>
2022-05-24 16:39:29 +0300

    Task1,2 lessen16 done. Task3 lessen16 in process.

---
+++ src/by/teachmeskills/lessen16/task2/Main.java
@@ -0,0 +1,14 @@
+package by.teachmeskills.lessen16.task2;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+//Написать регулярное выражение для валидации e-mail
+public class Main {
+    public static void main(String[] args) {
+        String email="igor1994s@yandex.by";
+        Pattern pat=Pattern.compile("^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,6}");
+        Matcher mat=pat.matcher(email);
+        System.out.println(mat.matches());
+    }
+}
